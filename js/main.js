910873d8b743ec002182504f5932c4c964fe6802
Wagner Paulo <wagstalos@hotmail.com>
2023-03-09 11:44:30 -0300

    refactory: code

--- js/main.js
+++ js/main.js
@@ -1,52 +1,70 @@
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  loop: false,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  breakpoints: {
-    600: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    900: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1200: {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-  },
-});
-
 AOS.init();
 
-const items = document.querySelectorAll(".accordion button");
+function swiperHome() {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    loop: false,
 
-function toggleAccordion() {
-  const itemToggle = this.getAttribute("aria-expanded");
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
 
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute("aria-expanded", "false");
-  }
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-  if (itemToggle == "false") {
-    this.setAttribute("aria-expanded", "true");
-  }
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    breakpoints: {
+      600: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      900: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    },
+  });
 }
 
-items.forEach((item) => item.addEventListener("click", toggleAccordion));
+function accordionHome() {
+  const items = document.querySelectorAll(".accordion button");
+  function toggleAccordion() {
+    const itemToggle = this.getAttribute("aria-expanded");
+
+    for (i = 0; i < items.length; i++) {
+      items[i].setAttribute("aria-expanded", "false");
+    }
+
+    if (itemToggle == "false") {
+      this.setAttribute("aria-expanded", "true");
+    }
+  }
+
+  items.forEach((item) => item.addEventListener("click", toggleAccordion));
+}
+
+function displayFullYear(element) {
+  const year = document.querySelector(element);
+  const getYear = new Date().getFullYear();
+
+  year.innerText = getYear;
+}
+
+const init = () => {
+  swiperHome();
+  accordionHome();
+  displayFullYear(".myDate");
+};
+
+init();
